2127c90cd9ba01c3a976b6e49d9927534f2f0c81
Paul Michalek <mich0044@umn.edu>
2016-02-11 17:22:58 -0600

    added properties and contraints to Account and Message classes

--- tweedledee/grails-app/domain/tweedledee/Account.groovy
+++ tweedledee/grails-app/domain/tweedledee/Account.groovy
@@ -2,6 +2,27 @@ package tweedledee
 
 class Account {
 
+    String handle
+    String name
+    String email
+    String password
+    Date dateCreated
+
+    static hasMany = [ followers : Account, following : Account, messages : Message ]
+    
+    static mapping = {
+        messages sort:'dateCreated'
+    }
     static constraints = {
+        handle nullable:false, unique:true
+        name nullable:false
+        email nullable:false,email:true,unique:true
+        password nullable:false,matches:"^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}\$"
+        /*
+            REGEX NOTE
+            Matches	asD1 | asDF1234 | ASPgo123
+            Non-Matches	 asdf | 1234 | ASDF12345
+         */
+
     }
 }
