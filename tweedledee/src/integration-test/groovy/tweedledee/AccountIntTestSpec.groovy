76c6512ba89d21249348ef10b17522dd416d2902
Sushant Dhakal <sushantdhakal@hotmail.com>
2016-02-13 00:37:27 -0600

    Integration Test A4

--- tweedledee/src/integration-test/groovy/tweedledee/AccountIntTestSpec.groovy
+++ tweedledee/src/integration-test/groovy/tweedledee/AccountIntTestSpec.groovy
@@ -1,6 +1,6 @@
 package tweedledee
 
-
+import grails.test.mixin.TestFor
 import grails.test.mixin.integration.Integration
 import grails.transaction.*
 import spock.lang.*
@@ -15,8 +15,32 @@ class AccountIntTestSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+
+    def 'Saving account with unique e-mail but non-unique handle'() {
+        setup:
+        def firstAccount = new Account(handle: 'TheRealSeanJohnson', name: 'Sean Johnson', email: 'johnsanSean@gmail.com', password: 'Orange1234')
+        def secondAcount = new Account(handle: 'TheRealSeanJohnson', name: 'Sean Johnson', email: 'seanJohnson@gmail.com', password: 'Orange1234')
+
+
+        when:
+        firstAccount.save(failOnError: true)
+        secondAcount.save(failOnError: true)
+
+        then:
+        println("Accounts have unique email and handle")
+    }
+
+    def 'Saving account with unique handle but non-unique email'() {
+        setup:
+        def firstAccount = new Account(handle: 'WillSmith', name: 'Sean Johnson', email: 'WillSmith@gmail.com', password: 'Orange1234')
+        def secondAcount = new Account(handle: 'TheRealWillSmith', name: 'Sean Johnson', email: 'WillSmith@gmail.com', password: 'Orange1234')
+
+
+        when:
+        firstAccount.save(failOnError: true)
+        secondAcount.save(failOnError: true)
+
+        then:
+        println("Accounts have unique email and handle")
     }
 }
