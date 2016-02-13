649d3565dcb65b9a09bfb196731442a6e299c403
Sushant Dhakal <sushantdhakal@hotmail.com>
2016-02-13 01:20:24 -0600

    Integration Test F2

--- tweedledee/src/integration-test/groovy/tweedledee/AccountIntTestSpec.groovy
+++ tweedledee/src/integration-test/groovy/tweedledee/AccountIntTestSpec.groovy
@@ -15,6 +15,11 @@ class AccountIntTestSpec extends Specification {
     def cleanup() {
     }
 
+    /**
+     * Test 1
+     * Requirement: A4
+     * Desc: Saving account with unique e-mail but non-unique handle
+     */
 
     def 'Saving account with unique e-mail but non-unique handle'() {
         setup:
@@ -30,6 +35,12 @@ class AccountIntTestSpec extends Specification {
         println("Accounts have unique email and handle")
     }
 
+    /**
+     * Test 2
+     * Requirement: A4
+     * Desc: Saving account with unique handle but non-unique email
+     */
+
     def 'Saving account with unique handle but non-unique email'() {
         setup:
         def firstAccount = new Account(handle: 'WillSmith', name: 'Sean Johnson', email: 'WillSmith@gmail.com', password: 'Orange1234')
@@ -42,5 +53,30 @@ class AccountIntTestSpec extends Specification {
 
         then:
         println("Accounts have unique email and handle")
+    }
+
+    /**
+     * Test 3
+     * Requirement: F2
+     * Desc: Two accounts may follow each other
+     */
+
+    def 'Two accounts may follow each other'() {
+        setup:
+        def firstAccount = new Account(handle: 'superman', name: 'Clark Kent', email: 'kent@krypton.com', password: 'Banana1234')
+        def secondAcount = new Account(handle: 'batman', name: 'Bruce Wayne', email: 'wayne@gotham.net', password: 'Orange1234')
+        firstAccount.following = [secondAcount] as Set<Account>
+        secondAcount.following = [firstAccount] as Set<Account>
+
+        when:
+        firstAccount.save(failOnError: true)
+        secondAcount.save(failOnError: true)
+
+        then:
+        print(firstAccount.name + "'s followers are: ")
+                firstAccount.following.each {follower -> println "${follower.name}"}
+
+        print(secondAcount.name + "'s followers are: ")
+        secondAcount.following.each {follower -> println "${follower.name}"}
     }
 }
