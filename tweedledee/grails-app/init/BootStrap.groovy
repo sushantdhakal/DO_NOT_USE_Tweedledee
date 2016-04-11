0ecd8880cc87c0e307ab4ab5e5834dd77d07478d
Sushant Dhakal <sushantdhakal@hotmail.com>
2016-04-10 20:57:44 -0500

    Updated Bootstrap to initialize the account and messages

--- tweedledee/grails-app/init/BootStrap.groovy
+++ tweedledee/grails-app/init/BootStrap.groovy
@@ -10,7 +10,25 @@ class BootStrap {
         def role = new Role(authority: 'ROLE_READ').save(flush: true, failOnError: true)
         new AccountRole(account: admin, role: role).save(flush: true, failOnError: true)
 
-       // def message1 = new Message(text: "Hello1234", dateCreated: new Date(), account: admin).save(flush: true, failOnError: true)
+        def admin1 = new Account(handle:'sushantdhakal',name:'sushantdhakal',password:'12345678pP',email:'sushant@admin.com').save(flush: true, failOnError: true)
+        def role1 = new Role(authority: 'ROLE_READ').save(flush: true, failOnError: true)
+        new AccountRole(account: admin1, role: role1).save(flush: true, failOnError: true)
+
+        def admin2 = new Account(handle:'paulM',name:'paulM',password:'12345678pP',email:'paul@admin.com').save(flush: true, failOnError: true)
+        def role2 = new Role(authority: 'ROLE_READ').save(flush: true, failOnError: true)
+        new AccountRole(account: admin2, role: role2).save(flush: true, failOnError: true)
+
+        (1..20).each {
+            msgNumber -> admin.addToMessages(text: "This my admin's message# $msgNumber").save(flush: true, failOnError: true)
+        }
+
+        (1..10).each {
+            msgNumber -> admin1.addToMessages(text: "This Sushant's message# $msgNumber").save(flush: true, failOnError: true)
+        }
+
+        (1..10).each {
+            msgNumber -> admin2.addToMessages(text: "This my Paul message# $msgNumber").save(flush: true, failOnError: true)
+        }
     }
 
     def destroy = {
